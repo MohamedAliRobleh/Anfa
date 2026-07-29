7c6cbf873a112da24775c92335f96bb4692ce88e
Mohamed Ali Robeh <medimadiba@gmail.com>
2026-07-28 21:46:15 -0400

    feat: add design tokens, fonts, and base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sea: '#14A69C',
+        'sea-deep': '#0B5C57',
+        sunlit: '#DDB067',
+        lavender: '#9C8FCB',
+        mist: '#E6F2EF',
+        sand: '#F7F4ED',
+        ink: '#1C2926',
+      },
+      fontFamily: {
+        display: ['Fraunces', 'Georgia', 'serif'],
+        body: ['"Hanken Grotesk"', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
